d6420b393270a032423d99d62c0c65a0bbb880cb
cs1086 <xtnact541@gmail.com>
2023-03-27 23:02:34 +0800

    first commit

--- app/src/main/java/com/example/firebase_authentication/MainActivity.kt
+++ app/src/main/java/com/example/firebase_authentication/MainActivity.kt
@@ -13,11 +13,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.firebase_authentication.ui.theme.FirebaseAuthenticationTheme
 import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            println("FirebaseAuth.getInstance()=${FirebaseAuth.getInstance().currentUser}")
             FirebaseAuthenticationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
